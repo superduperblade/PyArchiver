257e7f35eaf0409b135c36002b352ee5602be99b
SuperDuperBlade <85508905+SuperDuperBlade@users.noreply.github.com>
2025-09-03 12:52:09 +0200

    clean up

--- archivePage.py
+++ archivePage.py
@@ -7,22 +7,23 @@ class PageArchiver:
 
 
     def archivePage(self):
+      args = self.args
       with sync_playwright() as play:
         browser = play.chromium.launch(
-            headless = self.args.nHeadless,
-            executable_path= self.args.bexe,
+            headless = args.nHeadless,
+            executable_path= args.bexe,
             args=["--no-sandbox","--disable-dev-shm-usage"]
         )
 
         # waits until the page has loaded
         page = browser.new_page()
         print("Sending request to: "+self.url)
-        page.goto(self.url,wait_until="networkidle",timeout=self.args.timeout)
+        page.goto(self.url,wait_until="networkidle",timeout=args.timeout)
         
 
 
         if self.args.screenshot == True:
-           page.screenshot(path=self.args.outdir,full_page=True)
+           page.screenshot(path=args.outdir,full_page=True)
         
         if self.args.pdf == True:
            page.pdf(path=self.args.outdir)
