c406b5e5ee30e2834d9d268f4019f904601acdb8
SuperDuperBlade <85508905+SuperDuperBlade@users.noreply.github.com>
2025-09-03 20:44:14 +0200

    route handling

--- archivePage.py
+++ archivePage.py
@@ -1,16 +1,35 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Route, Request, Response
+
 import os
 import time
+from pathlib import Path
 class PageArchiver:
     def __init__(self,url,args):
         self.url = url
         self.args = args
 
 
+    def on_request(request):
+       
+       print("outbound -> ",request.url)
+    
+    def on_responce(request):
+       print("inbound -> "+request.url)
+
+
+    def handle_route(self,route: Route) -> None:
+       responce = route.fetch()
+
+       body = responce.text()
+       
+       print(body)
+
+       route.continue_()
+
     def archivePage(self):
       args = self.args
 
-      #auto populate
+      #todo auto populate
       filename = "example"
 
 
@@ -24,8 +43,21 @@ class PageArchiver:
         # waits until the page has loaded
         page = browser.new_page()
         print("Sending request to: "+self.url)
-        page.goto(self.url,wait_until="networkidle",timeout=args.timeout)
         
+        page.route("**/*",self.handle_route)
+        page.goto(self.url,wait_until="networkidle",timeout=args.timeout)
+    
+
+        
+
+
+        html = page.content()
+
+
+        #make sure folder exists
+        Path(args.outdir).mkdir(parents=True,exist_ok=True)
+        with open(os.path.join(args.outdir,filename+".html"),"w",encoding="utf-8") as file:
+           file.write(html)
 
 
         if args.screenshot == True:
